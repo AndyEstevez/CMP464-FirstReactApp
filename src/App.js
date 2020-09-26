e348ad69494744edc760441f1ed4b6d2d9f6861f
unknown <andyestevez27@gmail.com>
2020-09-26 18:22:26 -0400

    Final commit, I will become a React Pro

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/AndyEstevez"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check me out
         </a>
       </header>
     </div>
