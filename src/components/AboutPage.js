0d1583ed638d3048528ca2dbd9f2674a6af02d3f
unknown <andyestevez27@gmail.com>
2020-11-29 15:32:51 -0500

    commit error

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -5,11 +5,16 @@ class AboutPage extends Component {
     render() {
         return (
             <div>
-                <div>About Me: </div>
-                <div>Resume: </div>
-                <button>
+                <h2>About Me: </h2>
+                <h4 className="info">Born and raised in Bronx, NY, currently getting a B.S. in Computer Science from Lehman College.
+                    Learning front-end development with the React library. Interested in learning back-end development 
+                    frameworks and its programming languages.
+                </h4>
+                <br></br>
+                <h2>Resume: <button>
                    <Link className="download-btn" to="/Resume.pdf" target="_blank" download>Download</Link>
-                </button>
+                </button></h2>
+                
             </div>
         )
     }
--- src/components/ProjectPage/ProjectsPage.js
+++ src/components/ProjectPage/ProjectsPage.js
@@ -3,15 +3,21 @@ import { Link } from 'react-router-dom';
 import '../css/Project.css';
 
 class ProjectsPage extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            index: -1
+        }
+    }
     
     render() {
         return (
             <div className="proj-container">
-                <div>Projects: </div>
+                <h1>Projects: </h1>
                 {/* <div><a href={`/projects/${1}`}>Bitcoin App</a></div>
                 <div><a href={`/projects/${2}`}>Movie Site</a></div>
                 <div><a href={`/projects/${3}`}>Cryptocurrency Site</a></div> */}
-                <div className="proj1"><Link to={{pathname: '/projects/Bitcoin-App', state: {index: 0} }}>Bitcoin App</Link></div>
+                <div className="proj1"><Link to={{pathname: '/projects/Bitcoin-App', state: {index: this.state.index + 1} }}>Bitcoin App</Link></div>
                 <div className="proj2"><Link to={{pathname: '/projects/Movie-Site', state: {index: 1} }}>Movie Site</Link></div>
                 <div className="proj3"><Link to={{pathname: '/projects/Cryptocurrency-Site', state: {index: 2} }}>Cryptocurrency Site</Link></div>
             </div>
