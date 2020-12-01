dd8d1682a7d6bef17672265a8f286fccc07cd2a4
unknown <andyestevez27@gmail.com>
2020-12-01 16:48:40 -0500

    Fixed projects subpage

--- src/components/Navbar/NavbarObject.js
+++ src/components/Navbar/NavbarObject.js
@@ -6,16 +6,12 @@ class NavbarObject extends Component {
         return (
             <div>
                 <Navbar bg="primary" variant="dark" expand="lg">
-                    <Navbar.Brand>Andy Estevez</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
+                        <Nav className="mx-auto">
                             <Nav.Link href="/">Home</Nav.Link>
                             <Nav.Link href="/projects">Projects</Nav.Link>
                             <Nav.Link href="/about">About</Nav.Link>
                             <Nav.Link href="/contact">Contact Me</Nav.Link>
                         </Nav>
-                    </Navbar.Collapse>
                 </Navbar>
             </div>
         )
--- src/components/ProjectPage/ProjectsPage.js
+++ src/components/ProjectPage/ProjectsPage.js
@@ -1,25 +1,66 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 import '../css/Project.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Grid } from '@material-ui/core';
 
 class ProjectsPage extends Component {
     constructor(props){
         super(props);
         this.state = {
-            index: -1
+            index: [0, 1, 2],
+            0: { projectName: "Bitcoin App", 
+                    image: "https://raw.githubusercontent.com/AndyEstevez/CryptoApp/master/images/24hr_chart.png",
+                    description: "An android app that creates an account, view the current pricing of Bitcoin based on CoinAPI. See the historical pricing of Bitcoin by 24 Hour/1 Week/1 Month intervals and view buy/sell transactions the user creates which calculates their Net Cost.",
+                    URL: "https://github.com/AndyEstevez/CryptoApp", },
+            1: { projectName: "Movie Site", 
+                    image: "https://raw.githubusercontent.com/AndyEstevez/MovieSite/master/moviesite%20-%20screenshot.jpg",
+                    description: "Movie website presenting popular & top rated movies, search functionality, recommended movies, cast for movie. Used TMDB for the API, React for the front-end.",
+                    URL: "https://github.com/AndyEstevez/MovieSite", },
+            2: { projectName: "Cryptocurrency Site", 
+                    image: "https://raw.githubusercontent.com/AndyEstevez/CryptoSite/master/cryptosite-image.png",
+                    description: "Cryptocurrency website showing pricing and basic info of each one. Used CoinGecko for the API, React for the front-end.",
+                    URL: "https://github.com/AndyEstevez/CryptoSite", },
         }
     }
     
     render() {
+        console.log(this.state[0].image)
         return (
             <div className="proj-container">
-                <h1>Projects: </h1>
-                {/* <div><a href={`/projects/${1}`}>Bitcoin App</a></div>
-                <div><a href={`/projects/${2}`}>Movie Site</a></div>
-                <div><a href={`/projects/${3}`}>Cryptocurrency Site</a></div> */}
-                <div className="proj1"><Link to={{pathname: '/projects/Bitcoin-App', state: {index: this.state.index + 1} }}>Bitcoin App</Link></div>
-                <div className="proj2"><Link to={{pathname: '/projects/Movie-Site', state: {index: 1} }}>Movie Site</Link></div>
-                <div className="proj3"><Link to={{pathname: '/projects/Cryptocurrency-Site', state: {index: 2} }}>Cryptocurrency Site</Link></div>
+                <Grid container spacing={3} style={{  flex: "0 0 33.33%",  display: "flex", padding: "10px" }} justify="center">
+                {this.state.index.map(index => (
+                        <Card variant="outlined" style={{ maxHeight: "1300px", position:"relative", marginLeft:"600px", marginBottom: "10px", top:"10px", }}>
+                            <CardContent>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                    {this.state[index].projectName}
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    {this.state[index].description}
+                                </Typography>
+                            </CardContent>
+
+                            <CardActionArea>
+                                <CardMedia style={{  }}>
+                                    <img style={{ maxHeight: "400px", objectFit: "cover",  backgroundPosition: "center", backgroundCover: "cover" }} src={this.state[index].image} title={this.state[index].projectName}/>
+                                </CardMedia>
+                            </CardActionArea>
+                            
+                            <CardActions>
+                                <Button style={{marginLeft: "270px"}} variant="contained" color="primary" href={this.state[index].URL} target="_blank">
+                                    Link to GitHub
+                                </Button>
+                            </CardActions>
+                        </Card>
+                ))}
+                </Grid>
             </div>
         )
     }
